8e2a23374e761067e22b0ab8b13404d90dae8dca
ichiharu466-alt <ichiharu466@gmail.com>
2026-06-03 04:24:02 +0900

    Add PotentialNavigator for obstacle avoidance
    Implement a potential navigation node that uses odometry and laser scan data to navigate towards a goal while avoiding obstacles.

---
+++ ros_start/Nearlist_point.py
@@ -0,0 +1,129 @@
+import rclpy
+from rclpy.node import Node
+from geometry_msgs.msg import Twist
+from nav_msgs.msg import Odometry
+from sensor_msgs.msg import LaserScan
+from tf_transformations import euler_from_quaternion
+import math
+
+class PotentialNavigator(Node):
+    def __init__(self):
+        super().__init__("potential_nav_node")
+
+        #publisher, subscriber, timerの設定
+        self.publisher = self.create_publisher(Twist, "/sobit_pro/cmd_vel", 10)
+        self.create_subscription(Odometry, "/sobit_pro/odom", self.odom_callback, 10)
+        self.create_subscription(LaserScan, "/sobit_pro/scan", self.scan_callback, 10)
+        self.timer = self.create_timer(0.1, self.timer_callback)
+
+        # 現在地の取得と目標値の設定
+        self.pos_x, self.pos_y = 0.0, 0.0
+        self.goal_x, self.goal_y = 5.0, -1.0
+        self.yaw = 0.0
+        self.obstacles = []
+
+        #設定するパラメーターの値　A:引力の強さ　B:斥力の強さ　m:斥力の勾配の変化率
+        self.A, self.B, self.m = 0.5, 0.1, 2.0 
+
+    def odom_callback(self, msg):
+
+        #変数にodometory情報を代入
+        self.pos_x = msg.pose.pose.position.x
+        self.pos_y = msg.pose.pose.position.y
+
+        # quaternion取得
+        q = msg.pose.pose.orientation
+
+        # yawへ変換
+        _, _, self.yaw = euler_from_quaternion([q.x, q.y, q.z, q.w])
+
+        self.get_logger().info(f"x:{self.pos_x:.2f} y:{self.pos_y:.2f} yaw:{self.yaw:.2f}")
+
+    def scan_callback(self, msg):
+        
+        # 一番近い障害物だけを保持
+        self.obstacles = []
+        min_r = 2.0
+        nearest_point = None
+
+        for i, r in enumerate(msg.ranges):
+            if msg.range_min < r < 2.0:
+                angle = msg.angle_min + i * msg.angle_increment
+                x = r * math.cos(angle)
+                y = r * math.sin(angle)
+                
+                if r < min_r:
+                    min_r = r
+                    nearest_point = (x, y)
+        
+        if nearest_point:
+            self.obstacles = [nearest_point]
+
+    def timer_callback(self):
+
+        # ゴールとの距離(x軸方向:dx_g　y軸方向:dy_g)
+        dx_g, dy_g = self.goal_x - self.pos_x, self.goal_y - self.pos_y
+
+        # ゴールとの距離（直線的な）の計算
+        # ＜tips - max関数使う理由：0乗算の回避と数値の爆発を回避＞
+        dist_g = max(math.sqrt(dx_g**2 + dy_g**2), 0.1)
+        
+        # 引力の計算　(dx_g / dist_g)で正規化を行う　ゴールが遠いときに数値の爆発を防ぐため
+        # また教科書のように計算しないのは離れてても一定の引力でゴールにたどり着けるように
+        f_att_x = self.A * (dx_g / dist_g)
+        f_att_y = self.A * (dy_g / dist_g)
+
+        # 斥力の値を代入する変数を定義
+        f_rep_x, f_rep_y = 0.0, 0.0
+
+        # 各セクター内の重心の座標をox oyに代入してループ
+        for ox, oy in self.obstacles:
+
+            # robot座標 → world座標
+            wx = ox * math.cos(self.yaw) - oy * math.sin(self.yaw)
+            wy = ox * math.sin(self.yaw) + oy * math.cos(self.yaw)
+
+            do = math.sqrt(wx**2 + wy**2)
+
+            if 0.1 < do < 2.0:
+
+                force = (self.B * self.m) / (do**(self.m + 1))
+
+                f_rep_x -= force * (wx / do)
+                f_rep_y -= force * (wy / do)
+
+        # 引力と斥力それぞれの合成ベクトルの計算
+        att_mag = math.sqrt(f_att_x**2 + f_att_y**2)
+        rep_mag = math.sqrt(f_rep_x**2 + f_rep_y**2)
+
+        # それぞれの力の大きさを表示
+        self.get_logger().info(f"引力:{att_mag:.3f}, 斥力:{rep_mag:.3f}")
+
+        #引力と斥力の合計
+        total_x, total_y = f_att_x + f_rep_x, f_att_y + f_rep_y
+
+        msg = Twist()
+
+        #引力と斥力を合わせた合成ベクトル
+        norm = math.sqrt(total_x**2 + total_y**2)
+
+        # 速度出しすぎないための正規化
+        if norm > 0.5: 
+            total_x *= (0.5 / norm)
+            total_y *= (0.5 / norm)
+        
+        # world座標 → robot座標へ変換
+        cmd_x = total_x * math.cos(-self.yaw) - total_y * math.sin(-self.yaw)
+        cmd_y = total_x * math.sin(-self.yaw) + total_y * math.cos(-self.yaw)
+
+        msg.linear.x = float(cmd_x)
+        msg.linear.y = float(cmd_y)
+        self.publisher.publish(msg)
+
+#main関数
+def main():
+    rclpy.init()
+    node = PotentialNavigator()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
